775a1749461719c6d592f426b082948f6ac1519d
Rock26soft <74952168+Rock26soft@users.noreply.github.com>
2023-01-10 10:47:34 +0530

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDDNih-_EgGireOAeIbnlPkQyHo8qXlZoo",
+  authDomain: "venkat1satna.firebaseapp.com",
+  projectId: "venkat1satna",
+  storageBucket: "venkat1satna.appspot.com",
+  messagingSenderId: "138396132611",
+  appId: "1:138396132611:web:68cf2cae88eee09d05f536",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
